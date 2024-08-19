b5014b24577294146bc73b8d87c43c36d664b7ce
Kosal Nim <kosall123nim@gmail.com>
2024-08-19 16:28:50 +0700

    create new project

---
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/MyUploadVideoAuthorityApplication.java
@@ -0,0 +1,13 @@
+package com.kosal.my_upload_video_authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUploadVideoAuthorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUploadVideoAuthorityApplication.class, args);
+	}
+
+}
---
+++ my_upload_video_authority/src/test/java/com/kosal/my_upload_video_authority/MyUploadVideoAuthorityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosal.my_upload_video_authority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUploadVideoAuthorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
