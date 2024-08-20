aee6dd1043250bbc5c04645d62ae438fdd2dff7e
Kosal Nim <kosall123nim@gmail.com>
2024-08-20 16:01:19 +0700

    create AuthorMapper.java in this

---
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/mapper/AuthorMapper.java
@@ -0,0 +1,15 @@
+package com.kosal.my_upload_video_authority.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.kosal.my_upload_video_authority.entities.Author;
+import com.kosal.my_upload_video_authority.entities.DTO.AuthorDTO;
+
+@Mapper
+public interface AuthorMapper {
+	AuthorMapper INSTANCE=Mappers.getMapper(AuthorMapper.class);
+	
+	Author toAuthor(AuthorDTO authorDTO);
+	AuthorDTO authorDTO(Author author);
+}
