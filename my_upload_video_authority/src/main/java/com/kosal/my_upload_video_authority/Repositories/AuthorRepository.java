be3ecd87a1da505a11c361c74e2843ff46f1c504
Kosal Nim <kosall123nim@gmail.com>
2024-08-21 19:58:32 +0700

    Create Repositorie

---
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/Repositories/AuthorRepository.java
@@ -0,0 +1,15 @@
+package com.kosal.my_upload_video_authority.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.kosal.my_upload_video_authority.entities.Author;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author,Long>,JpaSpecificationExecutor<Author>{
+	List<Author>findByNameLike(String name);
+
+}
