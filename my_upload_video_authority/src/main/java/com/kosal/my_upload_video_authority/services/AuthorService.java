1f80d92bde697f87b4907f883be708e0b640b079
Kosal Nim <kosall123nim@gmail.com>
2024-08-23 20:06:00 +0700

    add Author getById(Long id),Author update(Long id,Author authorUpdate),List<Author> getAthor()and void remove(Long id)

--- my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/services/AuthorService.java
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/services/AuthorService.java
@@ -1,8 +1,14 @@
 package com.kosal.my_upload_video_authority.services;
 
+import java.util.List;
+
 import com.kosal.my_upload_video_authority.entities.Author;
 
 public interface AuthorService {
 	Author create(Author author);
+	Author getById(Long id);
+	Author update(Long id,Author authorUpdate);
+	List<Author> getAthor();
+	void remove(Long id);
 
 }
