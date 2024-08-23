b448a7461ee0953c50f0b86a9f2f020935e26347
Kosal Nim <kosall123nim@gmail.com>
2024-08-23 20:03:21 +0700

    create AuthorController.java

--- my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/controllers/AuthorController.java
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/controllers/AuthorController.java
@@ -1,7 +1,14 @@
 package com.kosal.my_upload_video_authority.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +32,33 @@ public class AuthorController {
 		Author author=AuthorMapper.INSTANCE.toAuthor(authorDTO);
 		Author createdAuthor = authorService.create(author);
 		return ResponseEntity.ok(AuthorMapper.INSTANCE.toAuthorDTO(createdAuthor));
+	}
+	@GetMapping("{id}")
+	public ResponseEntity<?>getOneAuthor(@PathVariable Long id){
+		Author author = authorService.getById(id);
+		return ResponseEntity.ok(AuthorMapper.INSTANCE.toAuthorDTO(author));
+		
+	}
+	@PutMapping("{id}")
+	public ResponseEntity<?>updateAuthor(@PathVariable("id") Long id,@RequestBody AuthorDTO authorDTO){
+		Author author = AuthorMapper.INSTANCE.toAuthor(authorDTO);
+		Author update = authorService.update(id, author);
+		return ResponseEntity.ok(AuthorMapper.INSTANCE.toAuthorDTO(update));
+			
+	}
+	@GetMapping
+	public ResponseEntity<?>getAuthors(){
+		List<AuthorDTO> aut = authorService.getAthor().stream()
+		.map(auth->AuthorMapper.INSTANCE.toAuthorDTO(auth))
+		.collect(Collectors.toList());
+		return ResponseEntity.ok(aut);
+		
+		
+	}
+	@DeleteMapping("{id}")
+	public ResponseEntity<?>delete(@PathVariable Long id){
+		authorService.remove(id);
+		return ResponseEntity.ok().build();
 	}
 
 }
