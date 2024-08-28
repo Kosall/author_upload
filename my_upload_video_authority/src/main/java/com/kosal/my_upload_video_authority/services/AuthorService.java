e984319b7bff8f9a868b91b086eb4486964a38c8
Kosal Nim <kosall123nim@gmail.com>
2024-08-28 16:18:28 +0700

    add Page<Author>getAuthors(Map<String, String>params)

--- my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/services/AuthorService.java
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/services/AuthorService.java
@@ -1,6 +1,9 @@
 package com.kosal.my_upload_video_authority.services;
 
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
 
 import com.kosal.my_upload_video_authority.entities.Author;
 
@@ -9,6 +12,8 @@ public interface AuthorService {
 	Author getById(Long id);
 	Author update(Long id,Author authorUpdate);
 	List<Author> getAthor();
+	List<Author>getAuthors(String name);
 	void remove(Long id);
+	Page<Author>getAuthors(Map<String, String>params);
 
 }
