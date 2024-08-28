ca21691123a8dd7aa9803b146bd93b4ea6f9f930
Kosal Nim <kosall123nim@gmail.com>
2024-08-28 16:17:08 +0700

    Change 'private String gender'to 'private Gender gender'

--- my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/entities/DTO/AuthorDTO.java
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/entities/DTO/AuthorDTO.java
@@ -1,12 +1,14 @@
 package com.kosal.my_upload_video_authority.entities.DTO;
 
+import com.kosal.my_upload_video_authority.entities.Gender;
+
 import lombok.Data;
 
 @Data
 public class AuthorDTO {
 	//private Long id;
 	private String name;
-	private String gender;
+	private Gender gender;
 	private int age;
 	private String phone;
 	private String email;
