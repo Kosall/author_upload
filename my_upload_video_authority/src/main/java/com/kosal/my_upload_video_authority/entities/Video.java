aa72d4c4bc07487dfe0cada7635c9667e2806aa7
Kosal Nim <kosall123nim@gmail.com>
2024-09-08 18:07:02 +0700

    create Video.java

--- my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/entities/Video.java
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/entities/Video.java
@@ -1,5 +1,28 @@
 package com.kosal.my_upload_video_authority.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="videos")
 public class Video {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="videos_id")
+	private Long id;
+	@Column(name="video_name")
+	private String name;
+	@ManyToOne
+	@JoinColumn(name="course_id")
+	private Course course;
 
 }
