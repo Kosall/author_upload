3e91ddb84e88ba30d762ff8a7dc67c5b71f6bc88
Kosal Nim <kosall123nim@gmail.com>
2024-09-08 18:04:43 +0700

    create CategoryRepository.java

---
+++ my_upload_video_authority/src/main/java/com/kosal/my_upload_video_authority/Repositories/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.kosal.my_upload_video_authority.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.kosal.my_upload_video_authority.entities.Category;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>,JpaSpecificationExecutor<Category> {
+	List<Category> findByNameLike(String name);
+	List<Category> findByNameContaining(String name);
+
+}
